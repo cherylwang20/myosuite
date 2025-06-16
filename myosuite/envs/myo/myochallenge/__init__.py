4c5a27383c93f7b1f38acec356ad5a2143efd12b
HUIYI WANG <huiyi.wang@mail.mcgill.ca>
2025-06-16 14:40:36 -0400

    new env for pingpong challenge

--- myosuite/envs/myo/myochallenge/__init__.py
+++ myosuite/envs/myo/myochallenge/__init__.py
@@ -30,6 +30,15 @@ def register_env_with_variants(id, entry_point, max_episode_steps, kwargs):
             silent=True
         )
 
+register_env_with_variants(id='myoChallengePingpong-v0',
+        entry_point='myosuite.envs.myo.myochallenge.pingpong_v0:PingPongEnvV0',
+        max_episode_steps=1000,
+        kwargs={
+            'model_path': curr_dir + '/../assets/arm/myoarm_tabletennis.xml',
+            'normalize_act': True,
+            'frame_skip': 5,
+        }
+    )
 
 register_env_with_variants(id='myoChallengeBimanual-v0',
         entry_point='myosuite.envs.myo.myochallenge.bimanual_v0:BimanualEnvV1',
